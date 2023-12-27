4b27aa4e624d88fd6e65d7b2c9e7d9e83d44581a
11023050A <154771252+11023050A@users.noreply.github.com>
2023-12-27 23:45:19 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://11023050a.github.io/soundcl//model.json';
 
 
 function preload() {
